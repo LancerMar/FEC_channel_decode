6fbf1418968654d3255e89c4a531e64f449b1f35
unknown <602545140@qq.com>
2025-12-07 20:05:44 +0800

    finish min-sum part

--- matlab/LDPC/BPSK_NRLDPC_sim.m
+++ matlab/LDPC/BPSK_NRLDPC_sim.m
@@ -7,7 +7,25 @@ max_iteration = 8;
 load Base_Matrics_5G\NR_1_0_16.txt
 Base_matric = NR_1_0_16;
 [rows_B,cols_B] = size(Base_matric);
-z = 16;
+z = 16; % expansion factor
+
+%number of none -1 block in base matrix
+none_minu1_B_len = sum(B(:)~=-1);
+% storage of none -1 block matrix
+% it is a linear storage, store every none -1 matrix
+R = zeros(none_minu1_B_len,z);
+% Row processing
+% 1. L-R(L: total belief of received bits)
+% 2. min-sum every row
+% 3. (L-R)+min-sum-update-value
+
+% temp regs for row values for min-sum(max 1s in one row)
+% get the row_weights
+row_weights = sum(Base_matric~=-1,2);
+max_row_weights = max(row_weights);
+temp_regs = zeros(max_row_weights,z);
+
+
 
 % number of message bits
 k = (cols_B - rows_B)*z;
@@ -34,13 +52,40 @@ received_word = symbols+sigma*randn(1,n);
 % SISO iteration message-passing layered decode
 LLR = received_word;
 iteration = 0; % times of iteration
+row_idx = 0;
 while iteration <max_iteration
     % each block layer is a layer
     % count of layer = rows of Base Matrix
     for layer = 1:rows_B
-        
+        temp_regs_idx = 0;
+        for col_idx = 1:cols_B
+            if Base_matric(layer,col_idx)~=-1
+                temp_regs_idx = temp_regs_idx+1;
+                row_idx = row_idx + 1;
+                % (L-R) 
+                LLR((col_idx-1)*z+1:col_idx*z) = LLR((col_idx-1)*z+1:col_idx*z) - R(row_idx,:);
+                % store in temp regs
+                temp_regs(temp_regs_idx,:) = mul_sh(LLR((col_idx-1)*z+1:col_idx*z),Base_matric(layer,col_idx));
+            end
+
+            % min-sum on temp regs in each layer
+            % process temp_regs(1:,col_temp_reg_idx)
+            for col_temp_reg_idx = 1:z 
+                [min1,pos] = min(abs(temp_regs(1:temp_regs_idx,col_temp_reg_idx)));
+                min2 = min(abs(temp_regs([1:pos-1 pos+1:temp_regs_idx],col_temp_reg_idx)));
+                sign_reg_i = sign(temp_regs(1:temp_regs_idx,col_temp_reg_idx));
+                parity = prod(sign_reg_i);
+
+                temp_regs(1:temp_regs_idx,col_temp_reg_idx) = min1; % abs value
+                temp_regs(pos,col_temp_reg_idx) = min2; % abs value
+
+                temp_regs(1:temp_regs_idx,col_temp_reg_idx) = parity*sign_reg_i.*temp_regs(1:temp_regs_idx,col_temp_reg_idx);% sign them
+            end
+
+        end
     end
     iteration = iteration+1;
+
 end
 
 
